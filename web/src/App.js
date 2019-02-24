b44bad19179edb6a758e029be4da08e4228389e6
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2019-02-23 23:07:35 -0500

    Implement color gradient

--- web/src/App.js
+++ web/src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import colorbrewer from 'colorbrewer';
+import statesJson from './states.json';
 import Map from './Map';
 import './App.css';
+
+function randint(end) {
+  return Math.floor(Math.random() * end);
+}
+
+const colors = colorbrewer.Blues[9];
 
 class App extends Component {
   state = {
     stateId: null,
-  };
+  }
 
   render() {
     return (
       <div className="App">
         {/* <img src={us2} alt="us map" /> */}
         <Map 
-          colors={{ 'CA': 'papayawhip' }}
+          colors={{}}
           onMouseOverState={stateId => this.setState({ stateId })}
           onMouseOutState={_ => this.setState({ stateId: null })}
         />
